327130d7dd2e1096c13888d7c31ce0bfb1a93670
Gabriel Euclides <Gabriel.Euclides@fit-tecnologia.org.br>
2018-12-07 10:02:06 -0200

    Improviment layout

--- src/assets/scripts/googleMaps/index.js
+++ src/assets/scripts/googleMaps/index.js
@@ -6,9 +6,9 @@ export default (function () {
     loadGoogleMapsAPI({
       key: 'AIzaSyDW8td30_gj6sGXjiMU0ALeMu1SDEwUnEA',
     }).then(() => {
-      const latitude  = 26.8206;
-      const longitude = 30.8025;
-      const mapZoom   = 5;
+      const latitude  = -23.5489;
+      const longitude = -46.6388;
+      const mapZoom   = 15;
       const { google }    = window;
 
       const mapOptions = {
